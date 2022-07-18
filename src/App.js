befd7277411462afcf3794237e1c812c7202b2a1
PatrickKibe <phantickibe01@gmail.com>
2022-07-18 17:43:18 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,19 +11,36 @@ const users = [
   { name: "Jane Doe", id: 2 },
   { name: "Billy Doe", id: 3 }
 ];
+const Users = users.map((user) => {
+  return <h1 key = {user.id}> {user.name} </h1>
+
+})
 
 // comment this out after completion and uncomment code below it to proceed
 function Child() {
-  return <div>This is children content</div>;
+  return <div>
+    <h1>This is children content</h1>
+    </div>;
 }
-/**
-  Challenge: Uncomment this code to complete quiz
+//child component
 
-function Child() {
+  //Challenge: Uncomment this code to complete quiz
+
+function child() {
+  function changeitUp() {
+    function handleChange (Event){
+      console.log (`${Event.target.value} ${Event.target.setValue}`);
+    }
+  }
+  
   return (
     <>
       <div>Child</div>
       <button>Change Parent Value</button>
+      
+      <select value = "filter" onChange >
+        <option value = "all"> Select a filter</option>
+        </select>
     </>
   );
 }
@@ -33,10 +50,19 @@ function Parent() {
     "I need to be updated from my child"
   );
 
+  function Parent () {
+    function handleSubmit(event){
+      event.preventDefault();
+      console.log("Submit");
+    }
+  }
+
   return (
     <>
       <h3>Update Parent State Challenge (Using Callback)</h3>
-      <div className="wrapper">
+
+  
+     <div className="wrapper">
         <div>Parent</div>
         <div className="box-wrapper">{value}</div>
       </div>
@@ -47,11 +73,10 @@ function Parent() {
     </>
   );
 }
-Uncomment this to tackle quiz
-**/
+//Uncomment this to tackle quiz
 
 // Comment out after completion
-function Parent() {
+function parent() {
   return (
     <div>
       <h3>Parent Component</h3>
@@ -59,6 +84,7 @@ function Parent() {
   );
 }
 // Comment above code after completion
+//parent component
 
 function App() {
   const [] = React.useState(true);
@@ -66,8 +92,8 @@ function App() {
     
     <>
     
-    <img src={logo} className="App-logo" alt="logo" />
-      <h3>User names</h3>
+    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2019-honda-civic-sedan-1558453497.jpg?crop=1xw:0.9997727789138833xh;center,top&resize=480:*" className="App-logo" alt="logo" />
+      <h3>JSX is cool!</h3>
       <ul></ul>
       <button>Hide Element Below</button>
 
